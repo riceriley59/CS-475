4e3ca3055bf990ca67dafaecfc95fae76488633e
Riley Rice <riceriley59@gmail.com>
2025-05-13 19:49:44 -0700

    Fixed incorrect CSV headers

--- project-4/run.sh
+++ project-4/run.sh
@@ -8,8 +8,6 @@ array_sizes=(1000 10000 50000 100000 500000 1000000 2000000 4000000 6000000 8000
 > "$output_file"
 
 make clean
-echo "Threads (#), Cities (#), Capitals (#), Performance (MegaCityCapitals/sec)" >> "$output_file"
-
 echo "Array Size (#), Non SIMD Mult (MegaMults/sec), SIMD Mult (MegaMults/sec), Speedup, Non SIMD Mult Sum (MegaMultSums/sec), SIMD Mult Sum (MegaMultSums/sec), Speedup" >> "$output_file"
 
 for arraysize in "${array_sizes[@]}";
